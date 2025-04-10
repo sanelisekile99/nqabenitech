0ee1eeb5b2e05236e00cb11b833d63324764f3cf
thegreatgabuza <thobanisgabuzam@gmail.com>
2025-04-10 23:27:22 +0200

    Update website content and assets

---
+++ nqabenitech-master/mediplus-lite/fonts/book-consultation.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAB0RIf6x4s7wm8YENeQEQZcPBVqEehM3g",
+    authDomain: "nqabeni-tech.firebaseapp.com",
+    projectId: "nqabeni-tech",
+    storageBucket: "nqabeni-tech.firebasestorage.app",
+    messagingSenderId: "903874696112",
+    appId: "1:903874696112:web:c6c3354d45300f79fefc75",
+    measurementId: "G-RLH6JP49Z0"
+  };
